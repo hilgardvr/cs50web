fefdc7fc1b079aa66b3392a376ebca76ea1be12b
Hilgard <hilgardvr@gmail.com>
2018-10-20 09:15:18 +0200

    working review adding to db - not checking for multiple reviews per user yet

--- project1/application.py
+++ project1/application.py
@@ -26,7 +26,8 @@ def index():
     currentUser = None
     if session['logged_in']:
         currentUser = session['logged_in'][0]
-    if not currentUser:
+        print (f"***** User: {currentUser}")
+    if currentUser == None:
         return render_template('index.html')
     else:
         return render_template('home.html', user=currentUser)
@@ -76,7 +77,28 @@ def search_results():
 @app.route("/book_details/<isbn>/<tit>/<author>/<year>", methods=['GET'])
 def book_results(isbn, tit, author, year):
     user = session['logged_in'][0]
-    our_reviews = ["review 1", "review 2"]
+    book_id = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn":isbn}).fetchone()
+    book_id = book_id.id
+    print (f"**** bookid: { book_id }")
+    our_reviews = []
+    if book_id:
+        our_reviews = db.execute("SELECT * FROM our_reviews WHERE book_id = :book_id", {"book_id":book_id}).fetchall()
+        print (our_reviews)
     res = requests.get("http://www.goodreads.com/book/review_counts.json", params={"key": "ob5yNrEgt6v1uC4ZtVumg", "isbns": isbn})
     print (res.json())
-    return render_template('book_details.html', user=user, isbn=isbn, title=tit, year=year, our_reviews=our_reviews)
+    return render_template('book_details.html', user=user, isbn=isbn, title=tit, author=author, year=year, our_reviews=our_reviews)
+
+@app.route("/create_review", methods=["POST"])
+def create_review():
+    reviewer = request.form['reviewer']
+    isbn = request.form['book']
+    rating = request.form['rating']
+    print(f"reviewer: {reviewer} isbn: {isbn} rating: {rating}")
+    user_id = db.execute("SELECT id FROM users WHERE username = :un", {"un": reviewer}).fetchone().id
+    book_id = db.execute("SELECT id FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone().id
+    review = request.form['review']
+    db.execute("INSERT INTO our_reviews (reviewer_id, review, book_id, rating) VALUES (:reviewer, :review, :book_id, :rating)",
+        {"reviewer":user_id, "review":review, "book_id":book_id, "rating":rating})
+    db.commit()
+    result = db.execute("SELECT * FROM our_reviews WHERE book_id = :book_id", {"book_id":book_id}).fetchall()
+    return render_template('home.html', result=result, user=session['logged_in'][0])
